c89263ed924ee8079cef932c1c30c16d6baa20e4
issacvp <ivlivingwithjesus@gmail.com>
2018-03-07 17:42:56 +0000

    @nebular auth and security module integrated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { CoreModule } from './@core/core.module';
+import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ThemeModule } from './@theme/theme.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NbEmailPassAuthProvider, NbAuthModule, NB_AUTH_TOKEN_CLASS, NbAuthJWTToken } from '@nebular/auth';
+import { getDeepFromObject } from '@nebular/auth/helpers';
+import { of as observableOf } from 'rxjs/observable/of';
+import { NbSecurityModule, NbRoleProvider } from '@nebular/security';
+import { RoleProvider } from './role.provider';
 
 @NgModule({
   declarations: [AppComponent],
@@ -26,10 +32,169 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     NgbModule.forRoot(),
     ThemeModule.forRoot(),
     CoreModule.forRoot(),
+
+    NbAuthModule.forRoot({
+      providers: {
+        email: {
+          service: NbEmailPassAuthProvider,
+          config: {
+            baseEndpoint: '',
+            login: {
+              alwaysFail: false,
+              rememberMe: true,
+              endpoint: '/api/auth/login',
+              method: 'post',
+              redirect: {
+                success: '/',
+                failure: null,
+              },
+              defaultErrors: ['Login/Email combination is not correct, please try again.'],
+              defaultMessages: ['You have been successfully logged in.'],
+            },
+            register: {
+              alwaysFail: false,
+              rememberMe: true,
+              endpoint: '/api/auth/register',
+              method: 'post',
+              redirect: {
+                success: '/',
+                failure: null,
+              },
+              defaultErrors: ['Something went wrong, please try again.'],
+              defaultMessages: ['You have been successfully registered.'],
+            },
+            logout: {
+              alwaysFail: false,
+              endpoint: '/api/auth/logout',
+              method: 'delete',
+              redirect: {
+                success: '/',
+                failure: null,
+              },
+              defaultErrors: ['Something went wrong, please try again.'],
+              defaultMessages: ['You have been successfully logged out.'],
+            },
+            requestPass: {
+              endpoint: '/api/auth/request-pass',
+              method: 'post',
+              redirect: {
+                success: '/',
+                failure: null,
+              },
+              defaultErrors: ['Something went wrong, please try again.'],
+              defaultMessages: ['Reset password instructions have been sent to your email.'],
+            },
+            resetPass: {
+              endpoint: '/api/auth/reset-pass',
+              method: 'put',
+              redirect: {
+                success: '/',
+                failure: null,
+              },
+              resetPasswordTokenKey: 'reset_password_token',
+              defaultErrors: ['Something went wrong, please try again.'],
+              defaultMessages: ['Your password has been successfully changed.'],
+            },
+            token: {
+              key: 'data.token',
+              getter: (module: string, res: HttpResponse<Object>) => getDeepFromObject(res.body,
+                this.getConfigValue('token.key')),
+            },
+            errors: {
+              key: 'data.errors',
+              getter: (module: string, res: HttpErrorResponse) => getDeepFromObject(res.error,
+                this.getConfigValue('errors.key'),
+                this.getConfigValue(`${module}.defaultErrors`)),
+            },
+            messages: {
+              key: 'data.messages',
+              getter: (module: string, res: HttpResponse<Object>) => getDeepFromObject(res.body,
+                this.getConfigValue('messages.key'),
+                this.getConfigValue(`${module}.defaultMessages`)),
+            },
+          },
+        },
+      },
+      forms: {
+        login: {
+          redirectDelay: 500, // delay before redirect after a successful login, while success message is shown to the user
+          provider: 'email',  // provider id key. If you have multiple providers, or what to use your own
+          rememberMe: true,   // whether to show or not the `rememberMe` checkbox
+          showMessages: {     // show/not show success/error messages
+            success: true,
+            error: true,
+          },
+        },
+        register: {
+          redirectDelay: 500,
+          provider: 'email',
+          showMessages: {
+            success: true,
+            error: true,
+          },
+          terms: true,
+        },
+        requestPassword: {
+          redirectDelay: 500,
+          provider: 'email',
+          showMessages: {
+            success: true,
+            error: true,
+          },
+        },
+        resetPassword: {
+          redirectDelay: 500,
+          provider: 'email',
+          showMessages: {
+            success: true,
+            error: true,
+          },
+        },
+        logout: {
+          redirectDelay: 500,
+          provider: 'email',
+        },
+        validation: {
+          password: {
+            required: true,
+            minLength: 4,
+            maxLength: 50,
+          },
+          email: {
+            required: true,
+          },
+          fullName: {
+            required: false,
+            minLength: 4,
+            maxLength: 50,
+          },
+        },
+      },
+    }),
+
+    NbSecurityModule.forRoot({
+      accessControl: {
+        guest: {
+          view: ['news', 'comments'],
+        },
+        user: {
+          parent: 'guest',
+          create: 'comments',
+        },
+        moderator: {
+          parent: 'user',
+          create: 'news',
+          remove: '*',
+        },
+      },
+    }),
+    
   ],
   bootstrap: [AppComponent],
   providers: [
     { provide: APP_BASE_HREF, useValue: '/' },
+    { provide: NB_AUTH_TOKEN_CLASS, useValue: NbAuthJWTToken },
+    { provide: NbRoleProvider, useClass: RoleProvider },
   ],
 })
 export class AppModule {
